aed1ba030d5418150a7f1693c89601fe289d99a1
rafael <rafaelhss@gmail.com>
2017-05-05 13:56:00 -0300

    vesrao inicial 3d

---
+++ img/Rafael Henrique_files/9i5lu8iOLfm.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["V0PYT"]); }
+
+__d('HideInlineHelp',['SelectorDeprecated'],(function a(b,c,d,e,f,g){var h={},i={registerMenu:function j(k,l,m){h[k]=l.setValue.bind(l,m);},triggerUndo:function j(k){h[k]();}};f.exports=i;}),null);
